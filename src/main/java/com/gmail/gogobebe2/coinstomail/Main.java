a93bac5d25a51dca2a0a13978325126ddde33de9
gogobebe2 <gogobebe2@gmail.com>
2016-02-27 16:21:13 +1300

    1.1.0-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/coinstomail/Main.java
+++ src/main/java/com/gmail/gogobebe2/coinstomail/Main.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.mail.*;
@@ -46,6 +47,7 @@ public class Main extends JavaPlugin {
             // Email variables:
             int numberOfCoins;
             HashMap<Integer, ? extends ItemStack> coins = player.getInventory().all(Material.EMERALD);
+            int coinsOwned = 0;
             String serverOwnerEmail = getConfig().getString("yourEmail");
             String paypalEmail;
             String username = getConfig().getString("username");
@@ -53,6 +55,8 @@ public class Main extends JavaPlugin {
             String host = getConfig().getString("host");
             String subject = "CoinsToMail cashout for " + player.getName();
             String text;
+
+            for (ItemStack coin : coins.values()) coinsOwned += coin.getAmount();
 
             try {
                 numberOfCoins = Integer.parseInt(args[0]);
@@ -62,18 +66,26 @@ public class Main extends JavaPlugin {
                 return true;
             }
 
+            if (numberOfCoins <= 0) {
+                player.sendMessage(ChatColor.RED + "Error! " + ChatColor.DARK_RED + args[0] + ChatColor.RED + " is not suitable number!");
+                return true;
+            }
+
             paypalEmail = args[1];
 
-            if (coins == null || numberOfCoins > coins.size()) {
+            if (numberOfCoins > coinsOwned) {
                 player.sendMessage(ChatColor.RED + "Error! You don't have " + numberOfCoins + " coins!");
                 return true;
             }
 
             text = "Coins: " + numberOfCoins + ", Paypal email: " + paypalEmail;
 
+            // debug:
+            removeCoins(numberOfCoins, player.getInventory());
+
             if (sendMail(serverOwnerEmail, paypalEmail, username, password, host, subject, text)) {
                 getLogger().log(Level.INFO, "Sent email '" + text + "' with the subject '" + subject + "'.");
-                for (int slot : coins.keySet()) player.getInventory().setItem(slot, null);
+                removeCoins(numberOfCoins, player.getInventory());
                 player.sendMessage(ChatColor.GREEN + "Cashout email has been sent with the email " + paypalEmail
                         + " for " + numberOfCoins + " coins.");
             }
@@ -82,6 +94,19 @@ public class Main extends JavaPlugin {
             return true;
         }
         return false;
+    }
+
+    private void removeCoins(int amount, PlayerInventory inventory) {
+        HashMap<Integer, ? extends ItemStack> coins = inventory.all(Material.EMERALD);
+
+        for (ItemStack coin : coins.values()) {
+            amount-= coin.getAmount();
+            if (amount <= 0) {
+                coin.setAmount(amount * -1);
+                break;
+            }
+            else coin.setAmount(0);
+        }
     }
 
     /**
