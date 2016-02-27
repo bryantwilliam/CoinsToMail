09f857a389f372ed2e7bb30e9779431b42a2aae6
gogobebe2 <gogobebe2@gmail.com>
2016-02-27 13:52:59 +1300

    1.0.2-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/coinstomail/Main.java
+++ src/main/java/com/gmail/gogobebe2/coinstomail/Main.java
@@ -19,6 +19,7 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Starting up " + this.getName() + ". If you need me to update this plugin, email at gogobebe2@gmail.com");
+        saveDefaultConfig();
     }
 
     @Override
